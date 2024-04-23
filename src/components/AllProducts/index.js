9f8e8f2802bceb82e948ab0f293d44d7b3157ab7
heony704 <heony704@gamil.com>
2024-04-23 10:45:34 +0900

    design: tablet 반응형 디자인 적용

--- src/components/AllProducts/index.js
+++ src/components/AllProducts/index.js
@@ -88,25 +88,21 @@ function AllProducts() {
     <section className="all-product">
       <div className="all-product-header">
         <h2 className="all-product-title">전체 상품</h2>
-        <div className="all-product-controls">
-          <div className="all-product-search">
-            <div className="all-product-search-icon">
-              <img src="images/ic_search.svg" alt="검색 아이콘" />
-            </div>
-            <input
-              className="all-product-search-input"
-              placeholder="검색할 상품을 입력해주세요"
-            />
+        <div className="all-product-search">
+          <div className="all-product-search-icon">
+            <img src="images/ic_search.svg" alt="검색 아이콘" />
           </div>
-          <Button to={PAGES.addProduct.link}>상품 등록하기</Button>
-          <div>
-            <DropDown
-              options={ORDERS}
-              selectedOption={order}
-              onSelectOption={onSelectOrderOption}
-            />
-          </div>
+          <input
+            className="all-product-search-input"
+            placeholder="검색할 상품을 입력해주세요"
+          />
         </div>
+        <Button to={PAGES.addProduct.link}>상품 등록하기</Button>
+        <DropDown
+          options={ORDERS}
+          selectedOption={order}
+          onSelectOption={onSelectOrderOption}
+        />
       </div>
       <ul className="all-product-list">
         {products.slice(0, numberOfProducts).map((product) => (
