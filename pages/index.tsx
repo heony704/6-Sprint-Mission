cfb3206b99d2ec6af51ff7c5401489b9e3fcd3a8
heony704 <heony704@gamil.com>
2024-06-06 22:16:58 +0900

    chore: tailwindcss 설치 및 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>HomePage</main>
+      <main>
+        <h1 className="text-3xl font-bold underline">HomePage</h1>
+      </main>
     </>
   );
 }
