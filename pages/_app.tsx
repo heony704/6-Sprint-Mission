39e97dcd9a4541c6ec561a35205582b87b089b3f
heony704 <heony704@gamil.com>
2024-06-08 01:05:02 +0900

    feat: html 메타 태그 전체 페이지에 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import Layout from '@components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>판다마켓</title>
+        <meta name="description" content="일상의 모든 물건을 거래해보세요" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />;
     </Layout>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>판다마켓</title>
-        <meta name="description" content="일상의 모든 물건을 거래해보세요" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
         <h1 className="text-3xl font-bold underline">HomePage</h1>
       </main>
