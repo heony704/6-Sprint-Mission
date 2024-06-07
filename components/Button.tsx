449a096538e37dd7a6d1c8461ef24fe3e334d894
heony704 <heony704@gamil.com>
2024-06-07 22:33:30 +0900

    feat: 버튼 컴포넌트 구현
    - prop 값에 따라 다른 스타일 구현

---
+++ components/Button.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  style?: {
+    shape?: 'square' | 'rounded';
+    size?: 'small' | 'large';
+  };
+  disabled?: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+export default function Button({
+  style = { shape: 'square', size: 'small' },
+  disabled = false,
+  onClick,
+  children,
+}: Props) {
+  return (
+    <button
+      className={`${style.size === 'small' ? 'h-[42px] px-6' : 'h-12 px-9'} ${style.shape === 'square' ? 'rounded-lg' : 'rounded-full'} flex cursor-pointer items-center justify-center bg-primary-400 text-base font-semibold text-white hover:bg-primary-600 active:bg-primary-700 disabled:cursor-default disabled:bg-gray-400`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
