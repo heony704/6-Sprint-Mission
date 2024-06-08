20507c6ab24cf6e3e39e95e3f7d37cc46bace119
heony704 <heony704@gamil.com>
2024-06-08 09:36:10 +0900

    feat: 게시글 상세 페이지 구현

---
+++ pages/boards/[id].tsx
@@ -0,0 +1,231 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useEffect, useState } from 'react';
+
+import { getBoardById, getBoardComments, postBoardComment } from '@apis/board';
+
+import Button from '@components/Button';
+
+import {
+  formatDateToTimeAgo,
+  formatDateToYYYYMMDD,
+} from '@utils/formatDateToString';
+
+interface Writer {
+  id: number;
+  nickname: string;
+  image?: string;
+}
+
+interface Board {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+  isLiked: boolean;
+  likeCount: number;
+  createdAt: string;
+  updatedAt: string;
+  writer: Writer;
+}
+
+interface Comment {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  writer: Writer;
+}
+
+export default function BoardItem() {
+  const [board, setBoard] = useState<Board | undefined>();
+  const [comments, setComments] = useState<Comment[] | undefined>();
+
+  const { isReady, query } = useRouter();
+  const id = Number(query.id);
+
+  useEffect(() => {
+    if (!isReady) return;
+
+    const loadBoardData = async (boardId: number) => {
+      const boardData: Board = await getBoardById(boardId);
+      setBoard(boardData);
+    };
+
+    const loadCommentsData = async (boardId: number) => {
+      const CommentsData: Comment[] = await getBoardComments(boardId);
+      setComments(CommentsData);
+    };
+
+    loadBoardData(id);
+    loadCommentsData(id);
+  }, [isReady]);
+
+  if (!board) return;
+
+  return (
+    <main className="m-auto px-4 pb-24 pt-8 md:px-6 xl:max-w-[1200px] xl:px-0">
+      <BoardDetail board={board} />
+      <div className="mt-10 md:mt-16">
+        <CommentForm boardId={id} />
+      </div>
+      <div className="mt-4 flex flex-col gap-4 md:mt-6">
+        {comments?.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+      </div>
+      <div className="mt-10 flex justify-center">
+        <BackButton />
+      </div>
+    </main>
+  );
+}
+
+type BoardDetailProp = {
+  board: Board;
+};
+
+function BoardDetail({ board }: BoardDetailProp) {
+  const { title, content, createdAt, isLiked, likeCount, writer } = board;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="relative flex flex-col gap-4 border-b">
+        <Image
+          src="/ic_kebab.svg"
+          width={24}
+          height={24}
+          className="absolute right-0 top-0"
+          alt="게시글 메뉴 아이콘"
+        />
+        <h3 className="pr-8 text-xl font-bold text-gray-800">{title}</h3>
+        <div className="mb-4 flex">
+          <div className="flex items-center gap-2 pr-4">
+            <Image
+              src="/img_default_profile.svg"
+              width={24}
+              height={24}
+              alt="글쓴이 프로필 이미지"
+            />
+            <p className="text-sm font-normal text-gray-600">
+              {writer.nickname}
+            </p>
+            <p className="text-xs font-normal text-gray-400">
+              {formatDateToYYYYMMDD(new Date(createdAt))}
+            </p>
+          </div>
+          <div className="flex items-center border-l pl-4">
+            <Image
+              src={`${isLiked ? '/ic_heart_full.svg' : '/ic_heart_empty.svg'}`}
+              width={24}
+              height={24}
+              alt={`${isLiked ? '꽉찬 하트' : '빈 하트'}`}
+            />
+            <p className="ml-1 text-sm font-normal text-gray-500">
+              {likeCount}
+            </p>
+          </div>
+        </div>
+      </div>
+      <pre className="text-base font-normal text-gray-800">{content}</pre>
+    </div>
+  );
+}
+
+type CommentFormProp = {
+  boardId: number;
+};
+
+function CommentForm({ boardId }: CommentFormProp) {
+  const [value, setValue] = useState<string>('');
+
+  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      await postBoardComment(boardId, value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="text-base font-semibold text-gray-800">댓글 달기</h3>
+      <textarea
+        className="h-[104px] resize-none rounded-xl bg-gray-100 px-6 py-4 text-base font-normal text-gray-800 placeholder:text-gray-400"
+        placeholder="댓글을 입력해주세요."
+        value={value}
+        onChange={handleTextareaChange}
+      />
+      <div className="flex justify-end">
+        <Button
+          style={{ shape: 'square', size: 'small' }}
+          disabled={value === ''}
+          onClick={handleButtonClick}
+        >
+          등록
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+type CommentProp = {
+  comment: Comment;
+};
+
+function Comment({ comment }: CommentProp) {
+  const { content, createdAt, writer } = comment;
+  return (
+    <div className="relative flex flex-col border-b pb-4 md:pb-6">
+      <Image
+        src="/ic_kebab.svg"
+        width={24}
+        height={24}
+        className="absolute right-0 top-0"
+        alt="댓글 메뉴 아이콘"
+      />
+      <p className="text-sm font-normal text-gray-800">{content}</p>
+      <div className="mt-4 flex items-center md:mt-6">
+        <Image
+          src={writer.image ?? '/img_default_profile.svg'}
+          width={32}
+          height={32}
+          className="max-h-8 rounded-full"
+          alt="댓글쓴이 프로필 이미지"
+        />
+        <div className="ml-2 flex flex-col">
+          <p className="text-xs font-normal text-gray-600">{writer.nickname}</p>
+          <p className="text-xs font-normal text-gray-400">
+            {formatDateToTimeAgo(new Date(createdAt))}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function BackButton() {
+  const { push } = useRouter();
+
+  return (
+    <Button
+      style={{ shape: 'rounded', size: 'large' }}
+      onClick={() => {
+        push('/boards');
+      }}
+    >
+      목록으로 돌아가기
+      <Image
+        src="/ic_back.svg"
+        width={24}
+        height={24}
+        className="ml-[10px]"
+        alt="뒤로가기 아이콘"
+      />
+    </Button>
+  );
+}
