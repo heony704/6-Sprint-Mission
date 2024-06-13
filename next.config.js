ba2abd9da01a1eb099258dfbafe9578c3c031025
heony704 <heony704@gmail.com>
2024-06-13 22:31:26 +0900

    fix: 이미지 오류 수정

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
         port: '',
-        pathname: '/Sprint_Mission/user/**',
+        pathname: '/**',
       },
       // FIXME: 오류를 피하기 위한 임시 허용
       {
